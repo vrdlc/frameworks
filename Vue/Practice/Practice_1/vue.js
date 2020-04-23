08ecc9458636a07892aae96b1fd94780ae84de05
vrdlc <virgilioramon.delacruz@gmail.com>
2020-04-23 08:42:26 -0700

    finally done with practice 1. Had a hard time keeping all the connections between the HTML and js file straight

--- Vue/Practice/Practice_1/vue.js
+++ Vue/Practice/Practice_1/vue.js
@@ -1,13 +1,29 @@
+Vue.component('practice1-comp', {
+	props: ['hobby'],
+  template: '<li v-on:click="removeHobby">{{ hobby }}</li>',
+  methods: {
+  	removeHobby: function() {
+    	this.$emit('hobbyremoved', this.hobby);
+    }
+  }
+});
+
 new Vue({
 	el: '#practice1',
-	data: {
-		name: 'Hobbies',
-		elements: ['farting', 'eating', 'running', 'smelling']
-	},
-	methods: {
-		addHobby: function() {
-
-			this.elements.push(document.getElementById('hobby').value)
-		}
-	}
-})
+  data: {
+  	elements: ['Sports', 'Cooking', 'Farting'],
+    userHobby: '',
+    hobbyWasDeleted: false
+  },
+  methods: {
+  	addHobby: function() {
+    	this.elements.push(this.userHobby);
+    },
+    removeHobby: function(hobby) {
+    	var position = this.elements.indexOf(hobby);
+			console.log(position + " pos")
+      this.elements.splice(position, 1);
+      this.hobbyRemoved = true;
+    }
+  }
+});
